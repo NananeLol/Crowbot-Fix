eec46f42b4cde542e1da75e8b117c74511c02224
NananeLol <160051209+NananeLol@users.noreply.github.com>
2025-08-14 18:09:27 +0200

    Update punch.js

--- commands/utilitaire/punch.js
+++ commands/utilitaire/punch.js
@@ -1,22 +1,47 @@
-const { MessageEmbed } = require('discord.js');
+const Discord = require('discord.js');
+const db = require('quick.db');
 
 module.exports = {
     name: 'punch',
-    description: 'Envoie un GIF de coup de poing',
-    execute(message, args) {
-        const gifs = [
-            'https://media.giphy.com/media/xUOwGdnPZ3r3D1c4uk/giphy.gif',
-            'https://media.giphy.com/media/3oEjI6SIIHBdRxXI40/giphy.gif',
-            'https://media.giphy.com/media/l1J9s6Gg5vQXk0J4c/giphy.gif'
-        ];
+    aliases: [], // tu peux mettre des raccourcis ici, ex: ["hit"]
 
-        const randomGif = gifs[Math.floor(Math.random() * gifs.length)];
+    run: async (client, message, args, prefix, color) => {
+        let perm = "";
+        message.member.roles.cache.forEach(role => {
+            if (db.get(`modsp_${message.guild.id}_${role.id}`)) perm = true;
+            if (db.get(`ownerp_${message.guild.id}_${role.id}`)) perm = true;
+            if (db.get(`admin_${message.guild.id}_${role.id}`)) perm = true;
+        });
 
-        const embed = new MessageEmbed()
-            .setColor('#ff0000')
-            .setTitle(`${message.author.username} donne un coup de poing !`)
-            .setImage(randomGif);
+        if (
+            client.config.owner.includes(message.author.id) ||
+            db.get(`ownermd_${client.user.id}_${message.author.id}`) === true ||
+            perm ||
+            db.get(`channelpublic_${message.guild.id}_${message.channel.id}`) === true
+        ) {
+            // Liste de gifs
+            const gifs = [
+                'https://media.giphy.com/media/xUOwGdnPZ3r3D1c4uk/giphy.gif',
+                'https://media.giphy.com/media/3oEjI6SIIHBdRxXI40/giphy.gif',
+                'https://media.giphy.com/media/l1J9s6Gg5vQXk0J4c/giphy.gif'
+            ];
+            const randomGif = gifs[Math.floor(Math.random() * gifs.length)];
 
-        message.channel.send({ embeds: [embed] });
+            // Si l'utilisateur mentionne quelqu'un
+            const target = message.mentions.users.first();
+            let description = `${message.author.username} donne un coup de poing ! 🥊`;
+            if (target) {
+                description = `${message.author.username} donne un coup de poing à ${target.username} ! 🥊`;
+            }
+
+            const embed = new Discord.MessageEmbed()
+                .setColor(color)
+                .setDescription(description)
+                .setImage(randomGif)
+                .setFooter(client.config.name)
+                .setTimestamp();
+
+            return message.channel.send({ embeds: [embed] });
+        }
     }
 };
